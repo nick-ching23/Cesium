001a6427abeb8936605f775e60cc9eb9d7facd11
Nicholas Ching <nick@p-a.co.za>
2024-10-15 15:49:53 -0400

    altered main method to read .ces files from src/main/resources

--- src/main/java/org/cesium/Main.java
+++ src/main/java/org/cesium/Main.java
@@ -1,18 +1,44 @@
 package org.cesium;
-
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.io.File;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
+
+    /*
+     * Main method will handle inputs from Cesium files with the suffix ".ces"
+     * These files will be run through our scanner program and tokenized appropriately.
+     * You can find these files in src/main/resources
+     */
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Usage: java org.cesium.Main <filename.ces>");
+            System.exit(1);
+        }
 
-        String inputProgram = "print \"Hello, World!\" + return || true <= 4.0 Reactive { x > 10 }";
+        String fileName = args[0];
+        File file = new File(fileName);
 
-        Scanner scanner = new Scanner(inputProgram);
-        List<Token> results = scanner.scanSourceCode();
+        try {
+            if (file.exists()) {
 
-        System.out.println(results);
+                // Read the contents of the file
+                String code = new String(Files.readAllBytes(file.toPath()));
+                Scanner scanner = new Scanner(code);
+                List<Token> tokens = scanner.scanSourceCode();
+
+                for (Token token : tokens) {
+                    System.out.println(token);
+                }
+            } else {
+                System.err.println("File not found: " + fileName);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + fileName);
+            e.printStackTrace();
+        }
 
     }
 }
