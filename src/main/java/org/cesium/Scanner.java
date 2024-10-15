53c59aa40a258ab586fb45354fe1eda0af38dfff
Nicholas Ching <nick@p-a.co.za>
2024-10-15 13:47:37 -0400

    added initial helper methods to identify char type

--- src/main/java/org/cesium/Scanner.java
+++ src/main/java/org/cesium/Scanner.java
@@ -1,4 +1,34 @@
 package org.cesium;
 
 public class Scanner {
+    private String input;
+    private int pos;
+    private int length;
+
+
+
+
+
+
+    // Helper Methods for identifying char type
+    private boolean isLetter(char c) {
+        return Character.isLetter(c);
+    }
+
+    private boolean isDigit(char c) {
+        return Character.isDigit(c);
+    }
+
+    private boolean isWhitespace(char c) {
+        return c == '\n' || c == '\t' || c == '\r' || c == ' ';
+    }
+
+    private boolean isDelimiter(char ch) {
+        return "()[]{};,".indexOf(ch) != -1;
+    }
+
+    private boolean isOperator(char c) {
+
+        return ("-+*=/<>|&!").indexOf(c) != -1;
+    }
 }
