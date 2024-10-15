ecbc06f31ae4a4008c568b78446d36ef1b6131c7
Nicholas Ching <nick@p-a.co.za>
2024-10-15 13:27:58 -0400

    made base scanner, token and tokentype classes. will fill out more

---
+++ src/main/java/org/cesium/Scanner.java
@@ -0,0 +1,4 @@
+package org.cesium;
+
+public class Scanner {
+}
---
+++ src/main/java/org/cesium/Token.java
@@ -0,0 +1,25 @@
+package org.cesium;
+
+public class Token {
+
+    private final TokenType type;
+    private final String value;
+
+    public Token(TokenType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + type + ", " + value + ">";
+    }
+}
